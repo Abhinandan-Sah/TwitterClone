d8a5fa7ac7a660f61fc74af249dac141f1cbd905
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-01 21:10:12 +0530

    initilizing the client side

---
+++ twitter-client/pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+
+
+export default function Home() {
+  return (
+    <>
+        <div className="grid grid-cols-12 h-screen w-screen">
+          <div className="col-span-3"></div>
+          <div className="col-span-6 "></div>
+          <div className="col-span-3"></div>
+        </div>
+    </>
+  );
+}
