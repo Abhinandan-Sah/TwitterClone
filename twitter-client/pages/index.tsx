ee67a2cb9daf99d71cff4a3ce52b966a9bba2da7
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-02 23:16:00 +0530

    update

--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
         <div className="grid grid-cols-12 h-screen w-screen">
           <div className="col-span-3"></div>
-          <div className="col-span-6 "></div>
+          <div className="col-span-6"></div>
           <div className="col-span-3"></div>
         </div>
     </>
