3b7e511719c3eb327fd0bb2fd7c5f689323a1dcf
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-12 20:56:42 +0530

    update

--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -1,2 +1,9 @@
-console.log("Hey, My name is Avi");
-console.log("Hey, My name is Avi");
+import { initServer } from "./app";
+
+async function init(){
+    const app =  await initServer();
+    app.listen(8000, () =>{
+        console.log("Server is running on port 8000");
+    })
+}
+init();
