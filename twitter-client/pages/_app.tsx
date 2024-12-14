4b22167a428a6fba8e2306690fb94e562c6daa40
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-14 22:53:30 +0530

    feat authentication

--- twitter-client/pages/_app.tsx
+++ twitter-client/pages/_app.tsx
@@ -1,12 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({subsets: ["latin"]});
 
+// const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+const clientId: string = process.env.NEXT_PUBLIC_CLIENT_ID as string;
+
+
+if (!clientId) {
+  throw new Error("NEXT_PUBLIC_CLIENT_ID is not set in the environment variables");
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <div className="Inter">
-<Component {...pageProps} />);
+    <GoogleOAuthProvider clientId={clientId}>
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
--- twitter-server/src/app/index.ts
+++ twitter-server/src/app/index.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import {expressMiddleware} from '@apollo/server/express4';
 import bodyParser from 'body-parser';
+import { PrismaClient } from '@prisma/client';
 
 export async function initServer(){
     const app = express();
     app.use(bodyParser.json());
+
+    // PrismaClient.User.create({
+
+    // })
+
     const graphqlServer = new ApolloServer({
         typeDefs:
         `type Query {
