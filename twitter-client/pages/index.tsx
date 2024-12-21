8ed0313acd75212b93d4a8fbd377c250d7015e6a
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-21 23:46:05 +0530

    update

--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -2,74 +2,87 @@ import Image from "next/image";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitterX } from "react-icons/bs";
 import { BiBell, BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
 import FeedCard from "@/components/FeedCard";
+import { GoogleLogin } from "@react-oauth/google";
 
-
-interface TwitterSidebarButton{
+interface TwitterSidebarButton {
   title: string;
   icon: React.ReactNode;
 }
 
 const SidebarMenuItems: TwitterSidebarButton[] = [
   {
-    title:'Home',
-    icon: <BiHomeCircle />
+    title: "Home",
+    icon: <BiHomeCircle />,
   },
   {
-    title:'Explore',
-    icon: <BiHash />
+    title: "Explore",
+    icon: <BiHash />,
   },
   {
-    title:'Notification',
-    icon: <BsBell />
+    title: "Notification",
+    icon: <BsBell />,
   },
   {
-    title:'Messages',
-    icon: <BsEnvelope />
+    title: "Messages",
+    icon: <BsEnvelope />,
   },
   {
-    title:'Bookmarks',
-    icon: <BsBookmark />
+    title: "Bookmarks",
+    icon: <BsBookmark />,
   },
   {
-    title:'Profile',
-    icon: <BiUser />
+    title: "Profile",
+    icon: <BiUser />,
   },
   {
-    title:'Profile',
-    icon: <BiUser />
+    title: "Profile",
+    icon: <BiUser />,
   },
   {
-    title:'Profileii',
-    icon: <BiUser />
-  }
-]
+    title: "Profileii",
+    icon: <BiUser />,
+  },
+];
 
-export default function Home(){
+export default function Home() {
   return (
     <>
-        <div className="grid grid-cols-12  h-screen w-screen px-56">
-          <div className="col-span-3 pt-8">
-            <div className="text-3xl h-fit hover:bg-gray-800 rounded-full p-2 w-fit cursor-pointer">
-            <BsTwitterX  />
-            </div>
-            <div className="mt-4 text-2xl">
-              <ul className="flex flex-col gap-y-2">
-                {SidebarMenuItems.map(item=>
-                  <li className="flex justify-start items-center gap-3 w-fit rounded-full px-2 py-2 cursor-pointer hover:bg-gray-800 transition-all" key={item?.title}><span>{item?.icon}</span><span>{item?.title}</span></li>
-                )}
-              </ul>
-              <div className="flex justify-center px-4 mt-3 font-semibold">
-              <button className="px-4 py-2 bg-blue-500 rounded-full w-full text-xl">Post</button>
-              </div>
+      <div className="grid grid-cols-12  h-screen w-screen px-56">
+        <div className="col-span-3 pt-8">
+          <div className="text-3xl h-fit hover:bg-gray-800 rounded-full p-2 w-fit cursor-pointer">
+            <BsTwitterX />
+          </div>
+          <div className="mt-4 text-2xl">
+            <ul className="flex flex-col gap-y-2">
+              {SidebarMenuItems.map((item) => (
+                <li
+                  className="flex justify-start items-center gap-3 w-fit rounded-full px-2 py-2 cursor-pointer hover:bg-gray-800 transition-all"
+                  key={item?.title}
+                >
+                  <span>{item?.icon}</span>
+                  <span>{item?.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-center px-4 mt-3 font-semibold">
+              <button className="px-4 py-2 bg-blue-500 rounded-full w-full text-xl">
+                Post
+              </button>
             </div>
           </div>
-          <div className="col-span-5 border-r-[1px] border-l-[1px] border-t-0 border border-gray-600">
-                <FeedCard />
-                <FeedCard />
-                <FeedCard />
-          </div>
-          <div className="col-span-3"></div>
         </div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] border-t-0 border border-gray-600">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div className="col-span-3 p-3">
+          <div className="p-5 rounded-lg bg-slate-700">
+            <h1 className="text-2xl font-semibold my-2">New to TwitterX?</h1>
+            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
