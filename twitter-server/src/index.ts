52aaa229bf85c0a12900e5eae9bb89a0a990e4c2
Abhinandan Sah <abhinandansah222@gmail.com>
2025-01-13 23:37:44 +0530

    up

--- twitter-server/src/index.ts
+++ twitter-server/src/index.ts
@@ -14,6 +14,8 @@ async function init(){
         console.log("Server is running on port 8000");
         console.log("Server is running on port 8000");
         console.log("Server is running on port 8000");
+        console.log("Server is running on port 8000");
+        console.log("Server is running on port 8000");
     })
 }
 init();
